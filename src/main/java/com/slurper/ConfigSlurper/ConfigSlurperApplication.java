92098ddca4a3c70030d5830721aef52d7329f2f2
digvijay17july <digvijay17july@gmail.com>
2020-03-27 23:43:18 +0530

    first commit

---
+++ src/main/java/com/slurper/ConfigSlurper/ConfigSlurperApplication.java
@@ -0,0 +1,13 @@
+package com.slurper.ConfigSlurper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSlurperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigSlurperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slurper/ConfigSlurper/ConfigSlurperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slurper.ConfigSlurper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSlurperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
